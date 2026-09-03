0167de279298d7ebda61f4e6d95574f6589fd84b
WINDBREAKASH <615918283@qq.com>
2026-09-03 20:24:40 +0800

    fix(extension): detect WhatsApp chat ids embedded in attributes

--- extensions/whatsapp-translation/src/whatsapp/selectors.ts
+++ extensions/whatsapp-translation/src/whatsapp/selectors.ts
@@ -1,8 +1,8 @@
 export const WHATSAPP_SELECTORS = {
   chatRoot: '#main',
   composer: '#main footer div[contenteditable="true"][data-tab]',
-  directIdentity: '#main[data-id$="@c.us"], #main [data-id$="@c.us"]',
-  groupIdentity: '#main[data-id$="@g.us"], #main [data-id$="@g.us"]',
+  directIdentity: '#main[data-id*="@c.us"], #main [data-id*="@c.us"]',
+  groupIdentity: '#main[data-id*="@g.us"], #main [data-id*="@g.us"]',
   incomingMessage: '#main .message-in',
   mediaMessage: '.media-panel',
   messageText: '.copyable-text .selectable-text',
